c4fb125e003a1b4e8418aa275ceaeaa7fcae1f27
kit <amit.sama772@gmail.com>
2015-06-22 02:15:39 +0200

    minor changes

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
     url(r'^sites/', views.show_all_sites),
-    url(r'^sites/(?P<pk>[0-9]+)/$', views.site_details),
+    url(r'^sites/(?P<pk>[0-9]+)/$', views.details),
     url(r'^summary/$', views.summary),
     url(r'^summary-average/$', views.summary_average),
 ]
--- demoapp/views.py
+++ demoapp/views.py
@@ -6,15 +6,15 @@ from django.db.models import Sum
 import sys
 
 def show_all_sites(request):
-	print "\n\nInside show_all_sites============================================================================================================="
+	#print "\n\nInside show_all_sites============================================================================================================="
 	sites = Site.objects.raw('SELECT * FROM demoapp_site GROUP BY `name` ORDER BY `created_date`')
 	return render(request, 'demoapp/show_all_sites.html',{'sites':sites})
 	
-	
-def site_details(request, pk):
-	print "Inside site_details %d=========================================================================================" %pk
-	sites = get_object_or_404(Site,pk=pk)	
-	return render(request,'demoapp/site_details.html',{'sites':sites})
+			
+def details(request, pk):
+	#print "Inside details %d=========================================================================================" %pk
+	sites = get_object_or_404(Site, pk=pk)	
+	return render(request, 'demoapp/site_details.html', {'sites':sites})
 	
 	
 def summary(request):
@@ -23,7 +23,5 @@ def summary(request):
 
 def summary_average(request):
 	sites = Site.objects.raw('SELECT id, name, AVG(a_val) AS a_v, AVG(b_val) AS b_v FROM demoapp_site GROUP BY `name`')
-	#for s in sites:
-	#	print "\nAMIT ++++++++++++++++++%s ----%.2f --- %.2f" %(s.name, s.a_v, s.b_v)
 	return render(request, 'demoapp/summary_average.html', {'sites':sites})
 # Create your views here.
